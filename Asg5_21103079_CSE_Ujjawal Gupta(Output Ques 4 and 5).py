8b5c54a2e03ebdc46a6b422897da3fb65d127c79
ug21103079 <96766463+ug21103079@users.noreply.github.com>
2022-03-07 23:46:44 +0530

    Add files via upload

---
+++ Asg5_21103079_CSE_Ujjawal Gupta(Output Ques 4 and 5).py
@@ -0,0 +1,20 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======== RESTART: C:\Users\HP\Desktop\Asg5_21103079_CSE_Ujjawal Gupta.py =======
+Question 4
+Enter The Numbers:
+45 1 4 2 3 9 8 7
+List Entered By User: [45, 1, 4, 2, 3, 9, 8, 7]
+Sorted List Using Merge Sort: [1, 2, 3, 4, 7, 8, 9, 45]
+Sorted List Using Quick Sort: [1, 2, 3, 4, 7, 8, 9, 45]
+Question 5
+Enter The Numbers:
+1 2 8 7 4 5 3 6 96 100 14 
+List Entered By User: [1, 2, 8, 7, 4, 5, 3, 6, 96, 100, 14]
+Sorted List: [1, 2, 3, 4, 5, 6, 7, 8, 14, 96, 100]
+Enter The Number You Want To Check:
+14
+If the Number is Present: True
+Number of times the number entered by user appeared: 1
+>>> 
